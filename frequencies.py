9a9682ad8380567d06ba422023703724bfe9230f
JanetThomas123 <115407383+JanetThomas123@users.noreply.github.com>
2022-11-02 21:22:00 +0000

    Update frequencies.py
    trying to do the solution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,18 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
+def remove_items(test_list, item):
+ 
+    # using filter() + __ne__ to perform the task
+    res = list(filter((item).__ne__, test_list))
+ 
+    return res
+ 
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    i = 0
+    while i < len(items):
+        count = items.count(items[i])
+        frequencies[items[i]] = count
+        items = remove_items(items, item[i])
     return frequencies
